8451a6eb0d6d4b631c5dee7c9ef08f269a5c1cd4
jjn <nets82PO>
2015-04-08 16:32:44 +0200

    Updated to use new Google Admin SDK api Moved configuration to file instead

--- README.md
+++ README.md
@@ -2,8 +2,8 @@ Google Apps Password Sync for Samba4
 ===========
 
 
-Reads from your Samba4 AD and updates changes password to Google Apps in SHA1 format.
-Note that this solution requires you to run:
+Reads from your Samba4 AD and updates passwords in Google Apps in SHA1 format.
+Note that this solution requires you to enable plaintext passwords:
 
 samba-tool domain passwordsettings set --store-plaintext=on
 
@@ -17,21 +17,41 @@ Python Dependencies
 - hashlib
 - syslog
 - samba
+- google api python client
 
-gdata can be downloaded from https://code.google.com/p/gdata-python-client/downloads/detail?name=gdata-2.0.17.zip&can=2&q=
+Google API must be installed with pip:
+pip install --upgrade google-api-python-client
 
 
 Install notes
 ===========
 
-1. Copy gaps.py and gapslib.py to desired locations.
-2. Run gaps.py in cron or at startup from rc.local, or both (if you wan't to schedule a restart). Change your settings in gapslib.py to fit your setup.
-3. Change syslog to desired local and add it to your syslog config for custom log file
-4. Start the daemon and watch log file for updates
+1. Install python-pip
+2. Create a project in Google API Console and add Admin SDK permission (read/write)
+3. Create a JSON Config for your project in Google Developer Console
+4. Install the JSON config to your samba machine in /etc/gaps/service.json (create the folder if missing)
+5. Copy gaps.py and gapslib.py to desired locations.
+6. Copy gaps.conf to /etc/gaps/gaps.conf and configure it
+7. Run gaps.py in cron or at startup from rc.local, or both (if you wan't to schedule a restart). Change your settings in gapslib.py to fit your setup.
+8. Change syslog to desired local and add it to your syslog config for custom log file
+9. Start the daemon and watch log file for updates
 
 
-If you are having trouble loading samba python modules please copy or symlink files and dirs in "/usr/local/samba/lib/python2.7/site-packages/" to "/usr/lib/python2.7/"
+* If you are having trouble loading samba python modules please copy or symlink files and dirs in "/usr/local/samba/lib/python2.7/site-packages/" to "/usr/lib/python2.7/"
+* If you are having issues with Google Permissions - you might need to add domain-wide authority to your service
+  Delegate domain-wide authority to your service account https://developers.google.com/drive/web/delegation#delegate_domain-wide_authority_to_your_service_account
 
 Debug
 ===========
-If the daemon don't start change /dev/null to dev/tty in gaps.py and watch for error messages.
+If the daemon don't start change /dev/null to /dev/tty in gaps.py and watch for error messages.
+
+
+Migration from old Google Provision API to new Google Admin SDK
+===========
+1. Install python-pip
+2. Create a project in Google Developer Console and ad Admin SDK permission
+3. Create a JSON config for you project in Google Developer Console
+4. Download the json config from the Google Developer Console to your samba machine
+5. pip install --upgrade google-api-python-client
+6. Copy your settings from your local version of gapslib.py to the new config file /etc/gaps/gaps.conf (create the fold if missing)
+7. Replace gapslib.py with the new one
--- gapslib.py
+++ gapslib.py
@@ -5,8 +5,13 @@ import sys
 import textwrap
 import hashlib
 import syslog
-import gdata.apps.multidomain.client;
+import json
+import httplib2
 import re
+
+from apiclient import errors
+from apiclient.discovery import build
+from oauth2client.client import SignedJwtAssertionCredentials
 
 from samba.credentials import Credentials
 from samba.auth import system_session
@@ -14,15 +19,11 @@ from samba.dcerpc import drsblobs
 from samba.ndr import ndr_unpack
 from samba.samdb import SamDB
 
-### Custom Settings ###
-gaDomain = "yourdomain.com"
-gaEmail = "adminuser@yourdomain.com"
-gaPassword = "yourpassword"
-sambaPrivate = "/usr/local/samba/private"
-sambaPath = "DC=YOURDOMAIN,DC=COM"
-adBase = "ou=Domain Users,dc=yourdomain,dc=com"
-### Set this option to True if using domain aliases ###
-replaceDomain = False
+from ConfigParser import SafeConfigParser
+
+## Get confgiruation
+config = SafeConfigParser()
+config.read('/etc/gaps/gaps.conf')
 
 ## Open connection to Syslog ##
 syslog.openlog(logoption=syslog.LOG_PID, facility=syslog.LOG_LOCAL3)
@@ -30,10 +31,23 @@ syslog.openlog(logoption=syslog.LOG_PID, facility=syslog.LOG_LOCAL3)
 ## Cached SHA 1 Passwords ##
 passwords = {}
 
-## Connect to Google ##
-client = gdata.apps.multidomain.client.MultiDomainProvisioningClient(domain=gaDomain)
-client.ssl = True
-client.ClientLogin(email=gaEmail, password=gaPassword, source='apps')
+## Load Google Configuration ##
+with open( config.get('google', 'service_json')) as data_file:
+  gaConfig = json.load(data_file)
+
+## Load Google Service ##
+def createDirectoryService(user_email):
+  credentials = SignedJwtAssertionCredentials(
+        gaConfig['client_email'],
+        gaConfig['private_key'],
+        scope='https://www.googleapis.com/auth/admin.directory.user',
+        sub=user_email
+  )
+
+  http = httplib2.Http()
+  http = credentials.authorize(http)
+
+  return build('admin', 'directory_v1', http=http)
 
 def esc(s):
     return quopri.encodestring(s, quotetabs=True)
@@ -45,28 +59,37 @@ def update_password(mail, pwd):
     pwd = pwd.encode('ascii', 'ignore')
     password = hashlib.sha1(pwd).hexdigest()
 
-    if replaceDomain:
-      mail = re.search("([\w.-]+)@", mail).group() + gaDomain
+    if config.get('common', 'replace_domain'):
+      mail = re.search("([\w.-]+)@", mail).group() + config.get('common', 'domain')
 
     if passwords.has_key(mail):
         if passwords[mail] == password:
             return 0
+
+    # Create a new service object
+    service = createDirectoryService(config.get('google', 'admin_email'))
+
     try:
-        user = client.RetrieveUser(mail)
+        user = service.users().get(userKey = mail).execute()
     except:
         syslog.syslog(syslog.LOG_WARNING, '[WARNING] Account %s not found' % mail)
         return 0
 
-    user.password = password
-    user.hash_function="SHA-1"
+    user['hashFunction'] = 'SHA-1'
+    user['password'] = password
+
     try:
-        client.UpdateUser(mail, user)
+        service.users().update(userKey = mail, body=user).execute()
         syslog.syslog(syslog.LOG_WARNING, '[NOTICE] Updated password for %s' % mail)
         passwords[mail] = password
     except:
         syslog.syslog(syslog.LOG_WARNING, '[ERROR] Could not update password for %s ' % mail)
 
 def run():
+    sambaPrivate = config.get('samba', 'private')
+    sambaPath = config.get('samba', 'path')
+    adBase = config.get('samba', 'base')
+
     creds = Credentials()
     samdb = SamDB(url=(sambaPrivate + "/sam.ldb.d/" + sambaPath + ".ldb"), session_info=system_session(), credentials=creds.guess())
     res = samdb.search(base=adBase, expression="(objectClass=user)", attrs=["supplementalCredentials", "sAMAccountName", "mail"])
